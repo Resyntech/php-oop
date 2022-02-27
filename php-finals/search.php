7bfacbd4bb1d51f7d6bced96eb77345ea65eb567
Gian Carlo Carranza <75870962+Resyntech@users.noreply.github.com>
2022-02-27 21:35:10 +0800

    wrong upload

--- php-finals/search.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if(isset($_POST['search'])) {
-        $search = $_POST['search'];
-
-        if(strlen(($search > 2)))
-            echo $search;
-    }
-?>
--- php-finals/tables/subjects.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE subjects(
-  subject_id int not null,
-  subject_name tinytext not null,
-  primary key(subject_id)
-);
